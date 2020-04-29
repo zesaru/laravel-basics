f20d8027e2bc436e7d7f24196d72d9958ac6b7db
Cesar <cesar@MacBook-Air-de-Cesar.local>
2020-04-29 22:27:08 +0900

    Qué es un CRUD y cómo implementarlo

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function destroy(User $user)
+    {
+        //
+    }
+}
